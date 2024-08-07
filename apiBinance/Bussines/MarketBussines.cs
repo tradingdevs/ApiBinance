1a8e61e4f0a7d3d02e468aa964684e8dc378aeeb
Administrador <olarrota.ing@outlook.com>
2024-08-06 18:54:26 -0600

    Agregar archivos de proyecto.

---
+++ apiBinance/Bussines/MarketBussines.cs
@@ -0,0 +1,31 @@
+﻿using Binance.Net.Enums;
+using Binance.Net.Interfaces.Clients;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiBinance.Bussines
+{
+    public class MarketBussines
+    {
+        private readonly IBinanceRestClient _binanceRestClient;
+
+        public MarketBussines(
+        IBinanceRestClient binanceRestClient)
+        {
+            _binanceRestClient = binanceRestClient ?? throw new ArgumentNullException(nameof(binanceRestClient));
+        }
+
+        public async Task GetKlines(string symbol) {
+            var klines = await _binanceRestClient.UsdFuturesApi.ExchangeData.GetKlinesAsync(symbol, KlineInterval.FiveMinutes, limit: 10);
+        
+            foreach (var item in klines.Data)
+            {
+                Console.WriteLine($"Tiempo Cierre:[{item.CloseTime}] Precio cierre: {item.ClosePrice.ToString()}");
+            }
+        }
+    }
+}
---
+++ apiBinance/Program.cs
@@ -0,0 +1,28 @@
+﻿using apiBinance.Bussines;
+using Binance.Net.Clients;
+using Binance.Net.Interfaces.Clients;
+using CryptoExchange.Net.Authentication;
+using Microsoft.Extensions.Logging;
+using System;
+
+internal static class Program
+{
+    private const string ApiKey = "GW0XwMVJY6BOGnpWVX9ZQcvsjVWRSli4AF1LmocK4edg8ecqRXKqgIoSQfS89Ml3";
+    private const string Secret = "vvyAZ0XheTGYIXHkVx1RWFoXyy61nTcPkSy7MYvmevCrMi4KYPxqxiSLPhwphHsH";
+    static async Task Main(string[] args)
+    {
+        var credentials = new ApiCredentials(ApiKey, Secret);
+        using IBinanceRestClient binanceRestClient = new BinanceRestClient(options => { options.ApiCredentials = credentials; });
+
+        Console.WriteLine("Prueba de conexión");
+        var pingAsyncResult = await binanceRestClient.UsdFuturesApi.ExchangeData.PingAsync();
+        Console.WriteLine($"Ping {pingAsyncResult.Data} ms");
+
+        MarketBussines market = new MarketBussines(binanceRestClient);
+        await market.GetKlines("LTCUSDT");
+
+        Console.WriteLine("Presione Enter para terminar");
+        Console.ReadLine();
+
+    }
+}
